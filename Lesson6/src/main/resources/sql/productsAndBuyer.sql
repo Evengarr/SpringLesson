285bcffef0c716151814477cf169e1d34203f1ad
Evengarr <72157480+Evengarr@users.noreply.github.com>
2021-07-03 22:51:31 +0300

    Update productsAndBuyer.sql

--- Lesson6/src/main/resources/sql/productsAndBuyer.sql
+++ Lesson6/src/main/resources/sql/productsAndBuyer.sql
@@ -7,17 +7,17 @@ CREATE TABLE products
     title VARCHAR(255),
     cost  double precision
 );
-INSERT INTO products (title)
-VALUES ('aaa'),
-       ('bbb'),
-       ('ccc'),
-       ('ddd'),
-       ('eee'),
-       ('fff'),
-       ('ggg'),
-       ('hhh'),
-       ('iii'),
-       ('jjj');
+INSERT INTO products (title,cost)
+VALUES ('aaa', 35.2),
+       ('bbb', 999.99),
+       ('ccc', 135.66),
+       ('ddd', 135.99),
+       ('eee', 235.15),
+       ('fff', 146.17),
+       ('ggg', 111.11),
+       ('hhh', 234.99),
+       ('iii', 946.94),
+       ('jjj', 964.66);
 
 DROP TABLE IF EXISTS buyers CASCADE;
 CREATE TABLE buyers
